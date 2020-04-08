e549ef133f1273f9b19b831c882c7f40041a88d3
Jacques Sham <jacquessham2015@gmail.com>
2020-04-07 18:12:16 -0700

    Fix example 3 and upload example 4

--- Part2/example3_slider.py
+++ Part2/example3_slider.py
@@ -4,12 +4,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-
+# Read file and split period into year and month
 data = pd.read_csv('Air_Traffic_Passenger_Statistics.csv')
 data['Activity Period'] = data['Activity Period'].astype(str)
 data['Year'] = data['Activity Period'].str[:4].astype(int)
 data['Month'] = data['Activity Period'].str[5:].astype(int)
 
+# Select columns
 selected_column = 'GEO Summary'
 data = data[['Published Airline','Year','Month','Passenger Count',
              'GEO Region',selected_column]]
@@ -18,16 +19,21 @@ data = data[['Published Airline','Year','Month','Passenger Count',
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# Layout is headline, description, graph, and slider
 app.layout = html.Div([
 	html.H1(children='Example 3: Slider'),
-	html.Div(children=['''
-			This example demostrate how to use slider on the dashboard.
+	html.Div(html.P(['''
+			This example demostrates how to use slider on the dashboard.''',
+			html.Br(),
+			'''
 			The chart display the observation of passenger traffic of 
 			each airline flying from/to a region in each month in a year.
-			The slider control the year.
+			The slider control the year.''',
+			html.Br(),
+			'''
 			This is not a good chart to visualize the data but the purpose
-			is to build a slider on this dashboard.
-		''']),
+			is to build a slider on this dashboard.'''
+		])),
     dcc.Graph(id='graph-output'),
     dcc.Slider(
     	id='year-slider',
@@ -39,13 +45,17 @@ app.layout = html.Div([
     ) # End Slider
 ]) # End Div
 
-# @app.callback is defined for the blow function
+# @app.callback is defined for the below function
 @app.callback(
 	Output('graph-output','figure'),[Input('year-slider','value')])
 def update_figure(selected_year):
+	# Filter the year selected in slider
 	df_filtered = data[data['Year']==selected_year]
+	# Each element in traces is for one value in selected_column
+	# Each value will be displayed as different colour
 	traces = []
 	for summ in df_filtered[selected_column].unique():
+		# Filter data points in filtered year and selected_column
 		df_filtered_temp = df_filtered[df_filtered[selected_column]==summ]
 		traces.append(dict(
 			x=df_filtered_temp['Month'],
@@ -60,13 +70,13 @@ def update_figure(selected_year):
 	        name=summ
 		))
 	
-
+	# return all info for figure
 	return {
 		'data': traces,
 		'layout': dict(
 			xaxis={'title': 'Month'},
 			yaxis={'title': 'Passenger Count', 'type': 'log'},
-			legend={'x':0, 'y': 0},
+			legend={'x':0, 'y': 0}, # Legend in bottom left
 			hovermode='closest',
 			transition= {'duration': 200}
 		)
--- Part2/example4_multinput.py
+++ Part2/example4_multinput.py
@@ -4,16 +4,113 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+
+# Read file and manipulate data
 data = pd.read_csv('Air_Traffic_Passenger_Statistics.csv')
 data['Activity Period'] = data['Activity Period'].astype(str)
 data['Year'] = data['Activity Period'].str[:4].astype(int)
 data['Month'] = data['Activity Period'].str[5:].astype(int)
-
 selected_column = 'GEO Summary'
-data = data[['Published Airline','Year','Month','Passenger Count',
-             selected_column]]
 
-
+# Dash Set up
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# Options Set up
+terminal_options = [{'label': i, 'value': i} for i in data['Terminal'].unique()]
+terminal_options.append({'label': 'All', 'value': '*'})
+geo_summ_options = [{'label': i, 'value': i} for i in 
+				          data['GEO Summary'].unique()]
+geo_summ_options.append({'label': 'All', 'value': '*'})
+
+app.layout = html.Div([
+	html.H1(children='Example 4: Multiple Input'), 
+	# Layout Position 0, headline
+	html.Div(html.P(['''
+		This example demostrates how to pass two inputs to the visualization.
+	''',
+	html.Br(),
+	'''
+		This example is the same as example 3 but adding two more filters.
+	''',
+	html.Br(),
+	'''
+		There are a lot of violation on data visualization rules, but
+		the purpose of this example is to demostrate the functionalities.
+		Sorry for the inconvenience. 
+	'''
+	])),  # Layout Position 1, description
+	html.Div([
+		html.Div([
+			dcc.Dropdown(
+				id='terminal-dropdown',
+				options=terminal_options,
+				value='Terminal 1'
+			)], style={'width':'40%', 'display': 'inline-block'}),
+		html.Div([
+			dcc.RadioItems(
+				id='geo-summary-radio',
+				options=geo_summ_options,
+				value='Domestic'
+			)
+		], style={'width':'40%', 'display': 'inline-block'})
+		# 'display' : 'inline-block' forces two div to be parallel
+	]), # End input Div
+	# Layout Position 2, Input options
+
+	dcc.Graph(id='vis'), # Layout Position 3, graph
+
+	dcc.Slider(
+		id='year-slider',
+    	min=data['Year'].min(),
+    	max=data['Year'].max(),
+    	value=data['Year'].min()+1,
+    	marks={str(year): str(year) for year in data['Year'].unique()},
+    	step=None
+	) # Layout Position 4, slider
+
+]) # End outter Div
+
+
+# Obtain 3 inputs from two dropdown lists and slider to produce one graph
+@app.callback(
+	Output('vis', 'figure'),
+	[Input('terminal-dropdown','value'), Input('geo-summary-radio','value'),
+	 Input('year-slider','value')])
+def update_figure(terminal, geo_summ, selected_year):
+	df_filtered = data[data['Year']==selected_year]
+	if geo_summ != '*':
+		df_filtered = df_filtered[df_filtered['GEO Summary']==geo_summ]
+	if terminal != '*':
+		df_filtered = df_filtered[df_filtered['Terminal']==terminal]
+
+	traces = []
+	for summ in df_filtered[selected_column].unique():
+		df_filtered_temp = df_filtered[df_filtered[selected_column]==summ]
+		traces.append(dict(
+			x=df_filtered_temp['Month'],
+			y=df_filtered_temp['Passenger Count'],
+			text=df_filtered_temp['Published Airline']+': Flying from/to '+\
+			     df_filtered_temp['GEO Region'] + ' (' +
+			     df_filtered['Terminal'] + ')',
+			mode='markers',
+			marker={
+	                'size': 15,
+	                'line': {'width': 0.5, 'color': 'white'}
+	            },
+	        name=summ
+		))
+
+	return {
+		'data': traces,
+		'layout': dict(
+			xaxis={'title': 'Month'},
+			yaxis={'title': 'Passenger Count', 'type': 'log'},
+			legend={'x':0, 'y': 0},
+			hovermode='closest',
+			transition= {'duration': 200}
+		)
+	}
+
+if __name__ == '__main__':
+	app.run_server(debug=True, port=8052)
