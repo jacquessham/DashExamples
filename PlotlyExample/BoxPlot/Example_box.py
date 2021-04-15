5acb474843472b469c643edfa8e1e61147303ea8
Jacques Sham <jacquessham2015@gmail.com>
2021-04-14 22:10:38 -0700

    Add script and data for box plot

---
+++ PlotlyExample/BoxPlot/Example_box.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import *
+
+
+# To initiate ploty to run offline
+init_notebook_mode(connected=True)
+# Import data
+df = pd.read_csv('salary.csv')
+
+data = []
+
+for school in df['school'].unique():
+	df_temp = df[df['school']==school]
+	data.append(go.Box(y=df['salary'], name=school))
+
+layout = dict(title={'text':'Alumni Salary across Schools', 'x':0.5},
+              barmode='group', xaxis=dict(tickmode='linear'))
+
+fig = go.Figure(data=data, layout=layout)
+
+plotly.offline.plot(fig, filename='box.html')
