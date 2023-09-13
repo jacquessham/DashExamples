c541c6ad2ea9d4e4fa25f47f4f075afe547fb13a
Jacques Sham <jacquessham2015@gmail.com>
2023-09-13 01:18:38 -0700

    Initiate sunburst charts

--- PlotlyExample/Readme.md
+++ PlotlyExample/Readme.md
@@ -123,4 +123,4 @@ You may find how to create candlestick charts with Plotly in the <a href="https:
 You may find how to create Choropleth graph with Plotly in the <a href="https://github.com/jacquessham/DashExamples/tree/master/PlotlyExample/ChoroplethGraph">Choropleth Graph folder</a>.
 
 ## Sunburst Charts
-Coming Soon...
+You may find how to create Sunburst Charts with Plotly in the <a href="https://github.com/jacquessham/DashExamples/tree/master/PlotlyExample/SunburstChart">Sunburst Charts folder</a>.
---
+++ PlotlyExample/SunburstChart/simplesunburst.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import *
+
+
+# To initiate ploty to run offline
+init_notebook_mode(connected=True)
+
+# Read data
+df = pd.read_csv('../Data/salary.csv')
+
+values = df['salary'].tolist() + [600000,350000]
+labels = df['name'].tolist() + df['school'].unique().tolist()
+parents = df['school'].tolist() + ['','']
+
+fig = go.Figure(go.Sunburst(
+    labels = labels,
+    values = values,
+    parents = parents,
+    textinfo = 'label+value',
+    marker_colorscale = 'Blues'))
+
+
+plotly.offline.plot(fig, filename='simple_sunburst.html')
